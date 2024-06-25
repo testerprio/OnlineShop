4ea8e914e48764c5d2143ab456c1a9db954b01e0
LENOVO <LENOVO@DESKTOP-PAESQ7A>
2024-06-25 11:02:29 +0700

    add searching testcase

---
+++ Object Repository/list filterProduct.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>list filterProduct</name>
+   <tag></tag>
+   <elementGuidId>8579d94c-58b7-4e7d-8b63-87780d88961a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@data-spm-anchor-id, 'searchlist.list')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
